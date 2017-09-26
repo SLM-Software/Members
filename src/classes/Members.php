f76e5d16f8bc9dfcc96ebbe0cc001dd553b98b79
Scott Yacko <scott@yackofamily.com>
2017-09-26 10:18:09 -0700

    Refactored getVersion

--- src/classes/Members.php
+++ src/classes/Members.php
@@ -6,7 +6,9 @@
  * if the size and complexity of the API warrants it's own class.
  *
  */
+namespace API;
 
+use Slim;
 /**
  * This is the Members parent class which contains non-complex methods.
  *
@@ -29,6 +31,23 @@ class Members
 	 * @var PDO $db The instance of the Postgresql PDO connect created at startup.
 	 */
 	protected $myDB;
+
+	/**
+	 * return the version of the API being called.
+	 *
+	 * @api
+	 *
+	 * @return array Keys: errCode, statusText, codeLoc, custMsg, retPack
+	 */
+	public function getVersion()
+	{
+		$client = new \GuzzleHttp\Client(['base_uri' => 'http://localhost:8080/slm/api/', 'timeout' => 2.0]);
+		$res = $client->request('GET', 'slminfo/version');
+		$retValue = substr($res->getBody(), 0);
+		$myObj = json_decode($retValue);
+		$resultString = array('errCode' => 0, 'statusText' => 'Success', 'codeLoc' => __METHOD__, 'custMsg' => '', 'retPack' => (array)$myObj->retPack);
+		return $resultString;
+	}
 
 	/**
 	 * Members constructor.
